08e7d1125d75f7741883978b4e8b99cb14298f44
Amak <58994783+amakheals@users.noreply.github.com>
2020-01-12 16:12:47 -0800

    try loading weakauras if found

--- Lib/Utils.lua
+++ Lib/Utils.lua
@@ -5,13 +5,19 @@ if AmakHealsTextures.BuildFail(80000) then return end
 local IsAddOnLoaded = IsAddOnLoaded
 
 -- check for optional dependency WeakAuras2
+if select(1, IsAddOnLoaded("WeakAuras")) then
+	loaded, reason = LoadAddOn("WeakAuras2")
+	if not loaded then
+		AmakHealsTextures.print("Can't Load Optional Dependencys: " .. "WeakAuras2")
+	end
+end
 local WeakAuras = WeakAuras
-if select(1, IsAddOnLoaded("WeakAuras2")) then
+if select(1, IsAddOnLoaded("WeakAuras")) then
 	if not WeakAuras then
 		AmakHealsTextures.print("Can't Find Optional Dependencys")
 	else
 		AmakHealsTextures.print("Found Optional Dependency: " .. "WeakAuras2")
-		WeakAuras.RegisterAddon(AddonName, AmakHealsTextures.PlainAddonTitle, "Data sourced from "..AmakHealsTextures.PlainAddonTitle, "Interface\\Addons\\"..AmakHealsTextures.name.."\\MiscTexturePack\\logo.blp")
+		WeakAuras.RegisterAddon(AddonName, AmakHealsTextures.PlainAddonTitle, "Data sourced from "..AmakHealsTextures.PlainAddonTitle, "Interface\\Addons\\" .. AmakHealsTextures.name .. "\\MiscTexturePack\\logo.blp")
 	end
 -- local spellsTextureMap = {}
 else
