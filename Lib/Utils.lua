4e97bc27f8f1396177219a67951eea0e968d1827
Amak <58994783+amakheals@users.noreply.github.com>
2020-01-12 21:21:14 -0800

    Version 2.3.8 - added throttling functions

--- Lib/Utils.lua
+++ Lib/Utils.lua
@@ -26,3 +26,57 @@ if select(1, IsAddOnLoaded("WeakAuras")) then
 else
 	AmakHealsTextures.print("Optional Dependency Not Loaded: " .. "WeakAuras")
 end
+
+-- DEBUG Code
+
+-- set aura throttle types
+AmakHealsTextures.throttleKeys = (AmakHealsTextures.throttleKeys or {})
+
+AmakHealsTextures.throttleKeys.Add("default")
+
+--set aura throttle states --
+AmakHealsTextures.throttleStates = (AmakHealsTextures.throttleStates or {})
+
+-- initialize a key for throttling
+function AmakHealsTextures.initThrottleTick(key) do
+	if not AmakHealsTextures.throttleKeys[key] then
+		AmakHealsTextures.throttleKeys[key] = 0
+		AmakHealsTextures.throttleStates[key] = false
+	end
+end
+
+function AmakHealsTextures.resetThrottleTick(resetkey, throttleRate) do
+	-- need to call AmakHealsTextures.initThrottleTick(key) first
+	if not AmakHealsTextures.throttleKeys[resetkey] then return end
+	local theTime = GetTime()
+	if (not AmakHealsTextures.lastTick) or (AmakHealsTextures.lastTick < (theTime - throttleRate)) then
+		AmakHealsTextures.lastTick = theTime
+	end
+	if (not AmakHealsTextures.throttleKeys[resetKey]) or (AmakHealsTextures.throttleKeys[resetKey] < (AmakHealsTextures.lastTick - 1)) then
+		AmakHealsTextures.throttleKeys[resetKey] = theTime
+		-- deactivate throttle
+		AmakHealsTextures.throttleStates[resetKey] = false
+	end
+	return
+end
+
+function AmakHealsTextures.shouldThrottle(throttlekey, throttleRate) do
+	-- need to call AmakHealsTextures.initThrottleTick(throttlekey) first
+	if not AmakHealsTextures.throttleKeys[throttlekey] then return end
+	-- if throttling active
+	if AmakHealsTextures.throttleKeys[throttlekey] then
+		if AmakHealsTextures.throttleStates[throttlekey] then
+			-- check and reset throttle if needed
+			AmakHealsTextures.resetThrottleTick(throttlekey, throttleRate)
+			-- throttle if needed
+			return AmakHealsTextures.throttleStates[throttlekey]
+		else
+			-- snooze next time
+			AmakHealsTextures.throttleStates[throttlekey] = true
+			-- and don't throttle yet
+			return false
+		end
+	end
+	-- no throttling
+	return false
+end
